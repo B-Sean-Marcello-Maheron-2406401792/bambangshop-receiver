dfa7d75874bceb39a7d2e6f7d53b060b26f4ae88
unknown <seanmarcello836@gmail.com>
2026-03-30 12:24:50 +0700

    Implement list function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -5,7 +5,7 @@ use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::service::notification::NotificationService;
 
-#[post("/subscribe/<product_type>")]
+#[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
@@ -13,7 +13,7 @@ pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     };
 }
 
-#[post("/unsubscribe/<product_type>")]
+#[get("/unsubscribe/<product_type>")]
 pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::unsubscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
@@ -25,6 +25,15 @@ pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
 pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
     return match NotificationService::receive_notification(notification.into_inner()) {
         Ok(f) => Ok(Json(f)),
+        Err(e) => Err(e)
+    };
+}
+
+#[get("/")]
+
+pub fn list() -> Result<Json<Vec<String>>> {
+    return match NotificationService::list_messages() {
+        Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
 }
